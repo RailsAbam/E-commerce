5dbfb9ad437a4e58d9273f094d1e71c43d318eaf
Rails Abam <railsdevabam@gmail.com>
2022-10-07 21:34:29 +0100

    new changes

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :elec_solutions
+
+  root "home#index"
+  get "home/batteries"
+  get "home/solar"
+  get "home/inverters"
+  get "home/install"
+  get "home/inquire"
+
+end
