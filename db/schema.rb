75a5e5dc99020e6f98502d400cfc97d2e4fc95a0
Rails Abam <railsdevabam@gmail.com>
2022-10-08 13:00:03 +0100

    commiting profiles changes and worked on the buttons and login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_192224) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_111715) do
   create_table "elec_solutions", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -29,6 +29,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_192224) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone"
+    t.string "state"
+    t.string "gender"
+    t.string "local_govt"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
